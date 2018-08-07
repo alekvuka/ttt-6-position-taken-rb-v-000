1f1f90550d799a2b26fe15cf53015dbce3c815ac
ALEKSANDAR VUKASINOVIC <alekvuka@gmail.com>
2018-08-07 22:39:22 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,18 @@
 # code your #position_taken? method here!
+def position_taken?(position)
+  if position == " "
+    return false 
+  elsif position == ""
+    return false 
+  elsif position == nil
+    return fasle 
+  elsif position == "X" || position == "O"
+    return true 
+  else 
+    return false 
+  end 
+end 
+
+    
+    
+  
