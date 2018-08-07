4486e1cda23f6a2bdba7703a872f66011642a58c
ALEKSANDAR VUKASINOVIC <alekvuka@gmail.com>
2018-08-07 22:47:20 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,8 +1,8 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
 
-  index = index.to_i - 1
-  character = board[index]
+  new_index = index.to_i - 1
+  character = board[new_index]
 
   if character == "X" || character == "O"
     return true
