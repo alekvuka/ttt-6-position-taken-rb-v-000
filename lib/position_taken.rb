504ea788595d5f7f9a76ac9a747b7069f93ad5ee
ALEKSANDAR VUKASINOVIC <alekvuka@gmail.com>
2018-08-07 22:42:40 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,12 +1,16 @@
 # code your #position_taken? method here!
-def position_taken?(position)
-  if position == " "
+def position_taken?(board, position)
+
+  position = position.to_i - 1
+  character = noard[position]
+
+  if character == " "
     return false
-  elsif position == ""
+  elsif character == ""
     return false
-  elsif position == nil
+  elsif character == nil
     return fasle
-  elsif position == "X" || position == "O"
+  elsif character == "X" || position == "O"
     return true
   else
     return false
